4d512e72e93985393454b92454cd3aeadabbc7b5
yaruliu <yarusophia@gmail.com>
2019-10-21 14:57:10 -0400

    switch between 2 datasets

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,7 +1,8 @@
 import * as d3 from 'd3'
 
 // const url = "https://udemy-react-d3.firebaseio.com/ages.json"
-const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json"
+const tallest_men = "https://udemy-react-d3.firebaseio.com/tallest_men.json"
+const tallest_women = "https://udemy-react-d3.firebaseio.com/tallest_women.json"
 const MARGIN = { TOP: 10, BOTTOM: 50, LEFT: 70, RIGHT: 10 }
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
@@ -9,77 +10,102 @@ const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 
 export default class D3Chart {
     constructor(element) {
-        const svg = d3.select(element)
+        const vis = this
+        vis.svg = d3.select(element)
           .append("svg")
             .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
             .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
           .append("g")
             .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
         
-        d3.json(url).then(data => {
-            const max = d3.max(data, d => d.height )
-            const min = d3.min(data, d => d.height )
+        // x label
+        vis.svg.append("text")
+            .attr("x", WIDTH / 2)
+            .attr("y", HEIGHT + 50)
+            .attr("text-anchor", "middle")
+            .text("The world's tallest men")
 
-            const y = d3.scaleLinear() 
-                .domain([min * 0.95, max])
-                .range([HEIGHT, 0])
+        vis.svg.append("text")
+            .attr("x", -(HEIGHT / 2))
+            .attr("y", -50)
+            .attr("text-anchor", "middle")
+            .text("Height in cm")
+            .attr("transform", "rotate(-90)")
 
-            const x = d3.scaleBand()
-                .domain(data.map(d => d.name))
-                .range([0, 800])
-                .padding(0.4)
-
-            const rects = svg.selectAll("rect")
-                .data(data)
-                
-            const xAxisCall = d3.axisBottom(x)
-            svg.append("g")
-                .attr("transform", `translate(0, ${HEIGHT})`)
-                .call(xAxisCall)
+        vis.xAxisGroup = vis.svg.append("g")
+            .attr("transform", `translate(0, ${HEIGHT})`)
+        
+        vis.yAxisGroup = vis.svg.append("g")
+        
+        Promise.all([
+            d3.json(tallest_men),
+            d3.json(tallest_women)
+        ]).then(datasets => {
+            const [men, women] = datasets
+            let flag = true
             
-            // x label
-            svg.append("text")
-                .attr("x", WIDTH / 2)
-                .attr("y", HEIGHT + 50)
-                .attr("text-anchor", "middle")
-                .text("The world's tallest men")
+            vis.data = men
+            vis.update()
 
-            svg.append("text")
-                .attr("x", -(HEIGHT / 2))
-                .attr("y", -50)
-                .attr("text-anchor", "middle")
-                .text("Height in cm")
-                .attr("transform", "rotate(-90)")
-
-            const yAxisCall = d3.axisLeft(y)
-            svg.append("g").call(yAxisCall)
-
-            rects.enter().append("rect")
-                .attr("x",  d => x(d.name))
-                .attr("y", d => y(d.height))
-                .attr("width", x.bandwidth)
-                .attr("height", d => HEIGHT - y(d.height))
-                .attr("fill", "grey")
-
+            d3.interval(() => {
+                vis.data = flag ? men : women
+                vis.update()
+                flag = !flag
+            }, 1000)
         })
-        
-        // d3.json(url).then(agesData => {
-        //     console.log(agesData)
-        //     const rects = svg.selectAll("rect")
-        //         .data(agesData)
-        
-        //     rects.enter()
-        //         .append("rect")
-        //             .attr("x", (d, i) => i * 100)
-        //             .attr("y", 50)
-        //             .attr("width", 50)
-        //             .attr("height", d => d.age + 5)
-        //             .attr("fill", d => {
-        //                 if (d.age > 10) return "red"
-        //                 else return "green"
-        //             })
-        // })
+    }
 
+    update() {
+        const vis = this
+        const data = vis.data
+        const max = d3.max(data, d => d.height )
+        const min = d3.min(data, d => d.height )
+
+        const y = d3.scaleLinear() 
+            .domain([min * 0.95, max])
+            .range([HEIGHT, 0])
+
+        const x = d3.scaleBand()
+            .domain(data.map(d => d.name))
+            .range([0, 800])
+            .padding(0.4)
+
+        const xAxisCall = d3.axisBottom(x)
+        vis.xAxisGroup.transition().duration(500).call(xAxisCall)
         
+        const yAxisCall = d3.axisLeft(y)
+        vis.yAxisGroup.transition().duration(500).call(yAxisCall)
+
+        // DATA JOIN
+        const rects = vis.svg.selectAll("rect")
+            .data(data)
+        
+        // EXIT
+        rects.exit()
+            .transition().duration(500)
+                .attr("height", 0)
+                .attr("y", HEIGHT)
+                .remove()
+
+        // UPDATE
+        rects.transition().duration(500)
+            .attr("x",  d => x(d.name))
+            .attr("y", d => y(d.height))
+            .attr("width", x.bandwidth)
+            .attr("height", d => HEIGHT - y(d.height))
+
+        // ENTER
+        rects.enter().append("rect")
+            .attr("x",  d => x(d.name))
+            .attr("width", x.bandwidth)
+            .attr("fill", "grey")
+            .attr("y", HEIGHT)
+            .transition().duration(500)
+                .attr("height", d => HEIGHT - y(d.height))
+                .attr("y", d => y(d.height))
+
+
+        console.log(rects)
+            
     }
 }
