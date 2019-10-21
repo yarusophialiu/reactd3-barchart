a7e27599303eb43140238b2793b10c20754aff2b
yaruliu <yarusophia@gmail.com>
2019-10-21 15:33:30 -0400

    pass props to children components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ChartWrapper from './ChartWrapper'
 import { Row, Col, Container } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar'
+import GenderDropdown from './GenderDropdown'
 
-function App() {
-  return (
-    <div className="">
-      <header className="App-header">
-      <Navbar bg="light">
-        <Navbar.Brand>Barchartly</Navbar.Brand>
-      </Navbar>
-      <Container>
-        <Row>
-          <Col xs={12}></Col>
-        </Row>
-        <Row>
-          <Col xs={12}><ChartWrapper /></Col>
-        </Row>
-      </Container>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    gender: "men"
+  }
+
+  genderSelected = (gender) => this.setState({ gender })
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <Navbar bg="light">
+            <Navbar.Brand>Barchartly</Navbar.Brand>
+          </Navbar>
+          <Container>
+            <Row>
+              <Col xs={12}><GenderDropdown genderSelected={this.genderSelected} /></Col>
+            </Row>
+            <Row>
+              <Col xs={12}><ChartWrapper gender={this.state.gender} /></Col>
+            </Row>
+          </Container>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
